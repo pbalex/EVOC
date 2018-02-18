91dc1bd51aff2ccda53964ff6d254823f1f6dd0f
pbalex <pbalex@umich.edu>
2018-02-18 13:27:36 -0500

    working reference card, still needs text

--- GUI.py
+++ GUI.py
@@ -5,6 +5,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.popup import Popup
 
 
 def send_input(command):
@@ -14,6 +15,12 @@ def send_input(command):
 	time.sleep(.05)
 	keyboard.release(command)
 	keyboard.press_and_release('alt+tab')
+
+def reference_callback(instance):
+		popup = Popup(title='Test popup',
+		content=Label(text='Hello world'),
+		size_hint=(None, None), size=(400, 400))
+		popup.open()
 
 def new_tab_callback(instance):
 	send_input('ctrl+t')
@@ -29,11 +36,14 @@ class Menu(GridLayout):
 		btn1 = Button(text='Open a new Tab')
 		btn1.bind(on_press=new_tab_callback)
 		self.add_widget(btn1)
+
 		btn2 = Button(text='Close Current Tab')
 		btn2.bind(on_press=close_tab_callback)
 		self.add_widget(btn2)
 
-		btn_reference = Button(text='Reference card')	
+		btn_reference = Button(text='Reference')
+		btn_reference.bind(on_press=reference_callback)
+		self.add_widget(btn_reference)
 
 
 class MyApp(App):
