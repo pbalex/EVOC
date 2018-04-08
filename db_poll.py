e4ca6841ee6b909805becccbf5d4a867d5236645
pbalex <pbalex@umich.edu>
2018-04-08 14:32:00 -0400

    search and find fixes

--- db_poll.py
+++ db_poll.py
@@ -4,7 +4,10 @@ import boto3
 import json
 import threading
 import ctypes
+import sys
+import subprocess
 import win32gui
+from win32con import SW_SHOW, SW_HIDE, HWND_NOTOPMOST, HWND_TOPMOST, SWP_NOMOVE, SWP_NOSIZE, SWP_SHOWWINDOW
 from boto3.dynamodb.conditions import Key, Attr
 from botocore.exceptions import ClientError
 
@@ -19,18 +22,32 @@ def send_input(command):
 
 #SPECIAL CASE CALLBACK FUNCTIONS
 
+def goto_callback(term):
+	find_callback(term)
+	select_callback()
+
+def find_callback(term):
+	send_input('ctrl+f')
+	time.sleep(.05)
+	keyboard.write(term)
+
+def search_callback(term):
+	send_input('ctrl+t')
+	time.sleep(.05)
+	keyboard.write(term)
+	send_input('enter')
+
+
 def select_callback():
 	send_input('esc')
 	time.sleep(.05)
 	send_input('enter')
 
 def switch_to_window(switch):
-	"""
-	Generates list of the hwnd of all 'real' windows.
-
-	Returns:
-	    (bool): List of hwnd of real windows.
-	"""
+	switch = switch.lower()
+	if (switch == "reference card" or switch == "reference"):
+		switch = "referencecard"
+	#Generates list of the hwnd of all 'real' windows.
 	def call(hwnd, param):
 		"""
 		The callback function to be used by EnumWindows.
@@ -43,9 +60,11 @@ def switch_to_window(switch):
 
 	for window in winds:
 		print (win32gui.GetWindowText(window))
-		if switch.lower() in win32gui.GetWindowText(window).lower():
+		if switch in win32gui.GetWindowText(window).lower():
 			print ("found window")
-			win32gui.SetActiveWindow(window)
+			win32gui.ShowWindow(window, SW_SHOW)
+			win32gui.BringWindowToTop(window)
+			send_input("alt")
 			win32gui.SetForegroundWindow(window)
 			break
 
@@ -78,7 +97,15 @@ def query_db():
 				if (i['COMMAND'] == "SELECT"):
 					select_callback()
 				elif (i['COMMAND'] == "OPEN REFERENCE"):
-					subprocess.run("python run_gui.py")
+					subprocess.run("python run_gui.py", shell=False)
+				elif (i['COMMAND'] == "SWITCH TO"):
+					switch_to_window(i['CUSTOM'])
+				elif (i['COMMAND'] == "CONTROL FIND"):
+					find_callback(i['CUSTOM'])
+				elif (i['COMMAND'] == "SEARCH"):
+					search_callback(i['CUSTOM'])
+				elif (i['COMMAND'] == "GOTO"):
+					goto_callback(i['CUSTOM'])
 				#all other basic commands
 				else:
 					try:
